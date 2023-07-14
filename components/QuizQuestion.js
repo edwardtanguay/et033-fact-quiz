48d019571363576e489e7ce96f5d15e2dfd8f01b
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-14 11:15:57 +0200

    showing choices

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -1,6 +1,22 @@
 export const QuizQuestion = (question) => {
-	return `
-	
+
+	const createChoicesHtml = () => {
+		let r = '';
+
+		r += `<ul class="choices">`; 
+		for (const choice of question.choices) {
+			r += `<li>${choice.label}</li>`
+		}
+		r += `</ul>`;
+
+		return r;
+	};
+
+	const choicesHtml = createChoicesHtml();
+
+	return /* html */ `
 	${question.text}
+
+	${choicesHtml}
 	`;
-}
+};
