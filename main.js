ef76a84fe784c6dbcddf6942cabda5288d7b9f1b
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-14 10:09:49 +0200

    questions and answers showing

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ import questions from './data/questions.json';
 
 const appTitle = "Unnützes Wissensquiz";
 
+let questionsHtml = '';
+for (const question of questions) {
+  questionsHtml += `<div class="question">${question.text}`;
+
+  questionsHtml += '<ul>';
+  for (const choice of question.choices) {
+    if (choice.isTheAnswer) {
+      questionsHtml += `<li class="correct">`;
+    } else {
+      questionsHtml += `<li>`;
+    }
+    questionsHtml += `${choice.label}</li>`;
+  }
+  questionsHtml += '</ul>';
+
+  questionsHtml += '</div>';
+}
+
 document.querySelector('#app').innerHTML = `
 <h1>${appTitle}</h1>
 
 <p>There are ${questions.length} questions.</p>
+<div class="questions">
+  ${questionsHtml}
+</div>
 `;
